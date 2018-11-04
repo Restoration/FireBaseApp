49a07818a846fa0ab5588fb1e7a0a4110b71a248
RyotArch <bboy.ryu@icloud.com>
2018-11-04 04:53:15 -0800

    [Add] express
    Add framework of express

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
-const functions = require('firebase-functions');
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccount.json");
-const request = require('request-promise');
-const data = require('./functions.json');
-const databaseUrl = data.database.url;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: databaseUrl
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-var db = admin.database();
-var ref = db.ref("room1");
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-ref.on("value", function(snapshot) {
-    console.log(snapshot.val());
-},
-function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
-} );
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
